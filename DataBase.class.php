18369c3d07d7bb119da0bb3dcb4e779c1821f6d4
Marcelo <marper.87@gmail.com>
2018-06-06 21:29:19 +0100

    CRUD methods

--- DataBase.class.php
+++ DataBase.class.php
@@ -15,6 +15,13 @@ class DataBase
 
 
     /**
+     * Array of reserved words
+     *
+     * @var array
+     */
+    protected $reservedWords = ['where', 'tablename', 'orderby', 'groupby', 'selectfields']; //allways lowercase
+
+    /**
      * Variável caminho BD
      * 
      * @var string
@@ -100,16 +107,235 @@ class DataBase
 
 
     /**
-     * Undocumented function
+     * Verifica a ligação à BD
      *
      * @return boolean
      */
     public function isConnected()
     {
-        return (boolean) $this->connection;
+        return (boolean)$this->connection;
 
     }
 
+    protected function unsetReservedWords(array $args)
+    {
 
+        $ret = [];
+
+        foreach ($args as $k => $v) {
+
+            if (!in_array(strtolower($k), $this->reservedWords)) {
+
+                $ret[$k] = $v;
+
+            }
+        }
+
+        return $ret;
+    }
+
+
+    public function insertQuery($args)
+    {
+
+        // insert into exemplo_pdo (campo1, campo2, campo3) VALUES ()
+        $table = $args['tablename'];
+
+        $query = "INSERT INTO " . $table;
+
+        $fields = $this->unsetReservedWords($args);
+
+        $fieldCount = 0;
+
+        foreach ($fields as $key => $value) {
+
+            if ($fieldCount === 0) {
+
+                $query .= " (" . $key;
+
+            } else {
+
+                $query .= ", " . $key;
+
+            }
+
+            $fieldCount++;
+
+        }
+
+
+
+
+        $query .= ") VALUES (";
+
+        $fieldCount = 0;
+
+        foreach ($fields as $key => $value) {
+
+            if ($fieldCount !== 0) {
+
+                $query .= ", ";
+
+            }
+
+            $query .= "'" . $value . "'";
+            $fieldCount++;
+        }
+
+        $query .= ")";
+
+        return $query;
+    }
+
+
+
+    public function updateQuery($args)
+    {
+
+        // update exemplo_pdo SET campo1 ="XXXX", campo2 = "XXXX", campo3 = "XXXX"
+        // Where 1=1 AND id = XX AND campo
+
+        $tablename = $args['tablename'];
+
+        $fields = $this->unsetReservedWords($args);
+
+        $query = "UPDATE " . $tablename . " SET ";
+
+        $fieldCount = 0;
+
+        foreach ($fields as $key => $value) {
+
+            if ($fieldCount === 0) {
+
+                $query .= $key . "= '" . $value . "' ";
+
+            } else {
+
+                $query .= "," . $key . "= '" . $value . "' ";
+
+            }
+
+            $fieldCount++;
+        }
+
+
+        $argsWhere = $args['where'];
+
+        if (count($args['where']) > 0) {
+
+            $query .= " WHERE 1=1";
+
+            foreach ($argsWhere as $key => $value) {
+
+                $query .= " AND " . $key . "= '" . $value . "'";
+            }
+
+        } else {
+
+            return false;
+        }
+
+        return $query;
+
+    }
+
+    public function deleteQuery($args)
+    {
+    //Delete from table where ....
+
+        $table = $args['tablename'];
+
+        $query = "DELETE FROM " . $table . " ";
+
+
+        $argsWhere = $args['where'];
+
+        if (count($args['where']) > 0) {
+
+            $query .= " WHERE 1=1";
+
+            foreach ($argsWhere as $key => $value) {
+
+                $query .= " AND " . $key . "= '" . $value . "'";
+            }
+
+        } else {
+
+            return false;
+        }
+
+        return $query;
+
+    }
+
+    public function selectQuery($args)
+    {
+        //SELECT campo1, campo2, campo3 FROM TABLE WHERE ID = XXX GROUP BY XXX ORDER BY XXX ASC
+
+        $table = $args['tablename'];
+
+        $selectFields = $args['selectFields'];
+
+        $query = "SELECT ";
+
+        $fieldCount = 0;
+
+        foreach ($selectFields as $value) {
+
+            if ($fieldCount === 0) {
+
+                $query .= $value;
+            } else {
+
+                $query .= ", " . $value;
+
+            }
+
+            $fieldCount++;
+        }
+
+        $query .= " FROM " . $table . " ";
+
+
+        $argsWhere = $args['where'];
+
+        if (count($args['where']) > 0) {
+
+            $query .= "Where 1=1 ";
+
+            foreach ($argsWhere as $key => $value) {
+                $query .= " AND " . $key . " = '" . $value . "'";
+            }
+
+        }
+
+        $argsGroupBy = $args['groupby'];
+
+        if (count($argsGroupBy) > 0) {
+
+            $query .= " GROUP BY ";
+
+
+
+            foreach ($argsGroupBy as $key => $value) {
+                if ($key === 0) {
+                    $query .= $value;
+                } else {
+
+                    $query .= ", " . $value;
+                }
+
+
+            }
+
+
+
+        }
+
+
+
+        return $query;
+
+    }
 
 }
